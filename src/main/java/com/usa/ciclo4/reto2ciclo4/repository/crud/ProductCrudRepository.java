be215f1feb28db3b395c50c20d982bc30c93657a
juanrodriguezmt <91926824+juanrodriguezmt@users.noreply.github.com>
2021-12-17 19:15:22 -0500

    Update ProductCrudRepository.java

--- src/main/java/com/usa/ciclo4/reto2ciclo4/repository/crud/ProductCrudRepository.java
+++ src/main/java/com/usa/ciclo4/reto2ciclo4/repository/crud/ProductCrudRepository.java
@@ -3,9 +3,12 @@ package com.usa.ciclo4.reto2ciclo4.repository.crud;
 import com.usa.ciclo4.reto2ciclo4.model.Product;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface ProductCrudRepository extends MongoRepository<Product,String> {
 
     public List<Product> findByPriceLessThanEqual(double precio);
+    @Query("{'description':{'$regex':'?0','$options':'i'}}")
+    public List<Vegetarian> findByDescriptionLike(String description);
 
 }
